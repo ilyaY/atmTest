3451664ff71bab8de129262913ad32564fdab58b
Ilya Yatsishin <ilya.yatsishin@jetbrains.com>
2014-04-18 19:47:48 +0200

    init

---
+++ src/main/java/delightex/client/client/AtmosphereTest.java
@@ -0,0 +1,10 @@
+package delightex.client.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class AtmosphereTest implements EntryPoint {
+  public void onModuleLoad() {
+    Window.alert("Loaded!");
+  }
+}
